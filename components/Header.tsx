ab397c5e2a809f93d0cc9a565c13b2f5156d44c9
FreeyW <888615@gmail.com>
2024-12-24 03:10:49 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -88,7 +88,7 @@ export default function Footer() {
                   title="IP.IM IP lookup"
                   className="inline-flex items-center w-full px-5 px-6 py-3 text-sm font-medium leading-4 text-white bg-gray-900 md:w-auto md:rounded-full hover:bg-gray-800 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-gray-800"
                 >
-                  GitHub
+                  IP.IM IP lookup
                 </a
                 >
               </div>
